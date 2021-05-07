51c577d86a79c6d2c86a6e772aa91e2c79fdd6bf
Gyubido <gubarcaro@gmail.com>
2021-05-07 00:31:35 -0300

    Desafio 2 - componentizando

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,46 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: Array<Genre>;
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+interface Genre {
+  id: number;
+  name:
+    | "action"
+    | "comedy"
+    | "documentary"
+    | "drama"
+    | "horror"
+    | "family";
+  title: string;
+}
+
+export function SideBar({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
